698e45943b73543bb462fa3f5206074a7a84621e
lupinekwakeup <99083504+lupinekwakeup@users.noreply.github.com>
2023-03-14 11:06:01 +0100

    Translated comments to English

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import time
 import re
 import ccxt
 
-# Longujeme v pripade, ze CPI je pozitivni
+# We long if CPI is positive
 def long():
     try:
-        exchange.create_market_order("BTCUSDT", "buy", 0.5) # Zadejte, jake coiny chcete longnout a kolik
-        exchange.create_market_order("ETHUSDT", "buy", 1) # Tento kod by longnul pul BTC a 1 ETH, tedy cca za $12k
+        exchange.create_market_order("BTCUSDT", "buy", 0.5) # Enter which coins you want to long and how much
+        exchange.create_market_order("ETHUSDT", "buy", 1) # This code would long half BTC and 1 ETH, roughly for $12k
         print("CPI came in positive - LONGED.")
     except Exception as e:
         print(f"Error during order creation: {e}")
 
-# Shortujeme v pripade, ze je CPI negativni
+# We short if CPI is negative
 def short():
     try:
-        exchange.create_market_order("BTCUSDT", "sell", 0.5) # To same, akorat se shortuje
-        exchange.create_market_order("ETHUSDT", "sell", 1) # Tzn. upravte si quantity a pripadne coiny. Muzete si je pridat i odebrat
+        exchange.create_market_order("BTCUSDT", "sell", 0.5) # Same thing, just shorting
+        exchange.create_market_order("ETHUSDT", "sell", 1) # So adjust the quantity and coins as necessary. You can add or remove them
         print("CPI came in negative - SHORTED.")
     except Exception as e:
         print(f"Error during order creation: {e}")
 
 exchange = ccxt.bybit({
-    'apiKey': 'xxxxx', # Zadejte do uvozovek vas API a secret key misto x
+    'apiKey': 'xxxxx', # Enter your API and secret key in quotes instead of x
     'secret': 'xxxxx'
 })
 
@@ -30,38 +30,31 @@ is_newest_tweet = "Not"
 
 while True:
     time.sleep(0.01)
-    # Vezmeme nejnovejsi Tweet od Tree News
+    # We take the newest tweet from Tree News
     tweet = next(twitter.TwitterSearchScraper('from:News_Of_Alpha').get_items())
     latest_tweet = tweet.rawContent
-    # Pokud nenalezneme novy Tweet, hledame znovu
+    # If we don't find a new tweet, we look again
     if latest_tweet == is_newest_tweet:
         continue
     is_newest_tweet = latest_tweet
-    # Hledame prvni cislo v Tweetu, ktere dle formatu ma byt mezirocni inflace
+    # We search for the first number in the tweet, which should be inter-year inflation according to the format
     match = re.search(r'\d+\.?\d*', is_newest_tweet)
     if match:
         inflation = float(match.group())
         print(inflation)
     else:
         print("No number found in the tweet")
-    # Zprava musi zacinat s U.S., jinak se take nejedna o CPI print
+    # The message must start with U.S., otherwise it's not a CPI print
     if is_newest_tweet.startswith("U.S."):
         print(f"This is a CPI print. Inflation is: {inflation}")
 
-        if inflation >= 6.4: # Zde upravime cislo podle ocekavane inflace
+        if inflation >= 6.4: # Here we adjust the number based on expected inflation
             short()
             break
 
-        if inflation <= 6: # Zde upravime cislo podle ocekavane inflace
+        if inflation <= 6: # Here we adjust the number based on expected inflation
             long()
             break
 
-        print("Wasnt worth trading.")
-    print("Tweet isnt CPI print. Waiting...")
-
-
-
-
-
-
-
+        print("Wasn't worth trading.")
+    print("Tweet isn't CPI print. Waiting...")
